75b46341a42e3b9361a4492a488878fb3971f659
Mats Estensen <mats.est@gmail.com>
2016-12-07 01:33:34 +0100

    add day 6

---
+++ 06/06-advent-part2.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env Python
+
+from collections import Counter
+
+def main():
+
+	with open("input.txt",'r') as f:
+		lines = f.readlines()
+
+	WIDTH = len(lines[0])
+
+	cols = [[] for i in range(WIDTH)]
+	print cols
+
+	for line in lines:
+		for i in range(WIDTH):
+			cols[i].append(line[i])
+
+	for col in cols:
+		print Counter(col).most_common()[-1][0]
+
+if __name__ == "__main__":
+    main()
