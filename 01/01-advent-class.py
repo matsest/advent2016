ebb7ca0442e58a9a1bf160f9a8c5a7d4d1875fcb
Mats Estensen <mats.est@gmail.com>
2016-12-23 02:45:58 +0100

    lint

--- 01/01-advent-class.py
+++ 01/01-advent-class.py
@@ -1,62 +1,62 @@
 #!/usr/bin/env Python
 
 class Player:
-	def __init__(self):
-		self.position = [0,0]
-		self.current_dir_id = 0
+    def __init__(self):
+        self.position = [0, 0]
+        self.current_dir_id = 0
 
-	def change_direction(self, new_direction):
-	 	if new_direction == 'L':
-	 		new_dir_id = (self.current_dir_id - 1) % 4
-	 	elif new_direction =='R':
-	 		new_dir_id = (self.current_dir_id + 1) % 4
-		self.current_dir_id = new_dir_id
+    def change_direction(self, new_direction):
+        if new_direction == 'L':
+            new_dir_id = (self.current_dir_id - 1) % 4
+        elif new_direction == 'R':
+            new_dir_id = (self.current_dir_id + 1) % 4
+        self.current_dir_id = new_dir_id
 
-	def move(self, direction, length):
-		x = self.position[0]
-		y = self.position[1]
+    def move(self, direction, length):
+        x = self.position[0]
+        y = self.position[1]
 
-		if direction == 0:
-			y += length
-		elif direction == 1:
-			x += length
-		elif direction == 2:
-			y -= length
-		elif direction == 3:
-			x -= length
-		self.position = [x,y]
+        if direction == 0:
+            y += length
+        elif direction == 1:
+            x += length
+        elif direction == 2:
+            y -= length
+        elif direction == 3:
+            x -= length
+        self.position = [x,y]
+
 
 def main():
+    with open("input.txt", 'r') as f:
+        dirs = f.read()
 
-	with open("input.txt",'r') as f:
-		dirs = f.read()
+    directions = ['N', 'E', 'S', 'W']
 
-	directions = ['N', 'E', 'S','W']
+    santas_helper = Player()
 
-	santas_helper = Player()
+    print "Starting to find Easter Bunny HQ:"
+    print "Current direction: ", santas_helper.current_dir_id
+    print "Current position: ", santas_helper.position
+    print "\n"
+    counter = 1
 
-	print "Starting to find Easter Bunny HQ:"
-	print "Current direction: ", santas_helper.current_dir_id
-	print "Current position: ", santas_helper.position
-	print "\n"
-	counter = 1
+    for direction in dirs.strip().split(', '):
+        rot = direction[0]
+        length = int(direction[1:])
+        print 'Direction #', counter
+        counter += 1
+        print 'Rotate: ', rot, ' Length: ', length
 
-	for direction in dirs.strip().split(', '):
-		rot = direction[0]
-		length = int(direction[1:])
-		print 'Direction #', counter
-		counter += 1
-		print 'Rotate: ', rot, ' Length: ', length
+        santas_helper.change_direction(rot)
 
-		santas_helper.change_direction(rot)
+        santas_helper.move(santas_helper.current_dir_id, length)
 
-		santas_helper.move(santas_helper.current_dir_id, length)
+        print "Current direction: ", directions[santas_helper.current_dir_id]
+        print "Current position: ", santas_helper.position
+        print "\n"
 
-		print "Current direction: ", directions[santas_helper.current_dir_id]
-		print "Current position: ", santas_helper.position
-		print "\n"
-
-	print "Length from start: ", abs(santas_helper.position[0]) + abs(santas_helper.position[1])
+    print "Length from start: ", abs(santas_helper.position[0]) + abs(santas_helper.position[1])
 
 if __name__ == "__main__":
     main()
